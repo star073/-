41a441223ed6f1015b6b323cff3f4d985d33debc
Your Namestar073 <1985.polyakova@mail.ru>
2024-03-03 17:19:14 +0400

    add file

---
+++ pet.py
@@ -0,0 +1,14 @@
+class Cat:
+    def __init__(self, name, floor, age):
+        self.name = name
+        self.floor = floor
+        self.age = age
+
+    def getName(self):
+        return self.name
+
+    def getFloor(self):
+        return self.floor
+
+    def getAge(self):
+        return self.age
---
+++ testPet.py
@@ -0,0 +1,13 @@
+from pet import Cat
+
+cat1 = Cat("Барон", "мальчик", "2")
+cat2 = Cat("Сэм", "мальчик", "2")
+
+print("cat1.name:", cat1.name)
+print("cat1.floor:", cat1.floor)
+print("cat1.age:", cat1.age)
+print("cat2.name:", cat2.name)
+print("cat2.floor:", cat2.floor)
+print("cat2.age:", cat2.age)
+print("cat1.getName=", cat1.getname)
+print("cat2.getName=", cat2.getname)
